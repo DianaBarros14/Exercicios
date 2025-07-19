f4b9035160afd7004321e3e355bb4204206c2415
DianaBarros14 <diana.barros14@gmail.com>
2025-07-19 11:22:11 -0300

    terceiro formulario

---
+++ Exercicio3/Exercicio3/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Exercicio3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRsocial = new System.Windows.Forms.TextBox();
+            this.txtNfantasia = new System.Windows.Forms.TextBox();
+            this.txtObs = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mtbCnpj = new System.Windows.Forms.MaskedTextBox();
+            this.mtbTel = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // txtRsocial
+            // 
+            this.txtRsocial.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRsocial.Location = new System.Drawing.Point(270, 80);
+            this.txtRsocial.Name = "txtRsocial";
+            this.txtRsocial.Size = new System.Drawing.Size(408, 30);
+            this.txtRsocial.TabIndex = 0;
+            // 
+            // txtNfantasia
+            // 
+            this.txtNfantasia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNfantasia.Location = new System.Drawing.Point(271, 126);
+            this.txtNfantasia.Name = "txtNfantasia";
+            this.txtNfantasia.Size = new System.Drawing.Size(407, 30);
+            this.txtNfantasia.TabIndex = 1;
+            // 
+            // txtObs
+            // 
+            this.txtObs.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtObs.Location = new System.Drawing.Point(271, 256);
+            this.txtObs.Multiline = true;
+            this.txtObs.Name = "txtObs";
+            this.txtObs.Size = new System.Drawing.Size(408, 30);
+            this.txtObs.TabIndex = 4;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(411, 316);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(131, 44);
+            this.btnMostrar.TabIndex = 5;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Razão social";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(109, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nome fantasia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(109, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "CNPJ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Telefone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(109, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Observação";
+            // 
+            // mtbCnpj
+            // 
+            this.mtbCnpj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbCnpj.Location = new System.Drawing.Point(270, 170);
+            this.mtbCnpj.Name = "mtbCnpj";
+            this.mtbCnpj.Size = new System.Drawing.Size(408, 30);
+            this.mtbCnpj.TabIndex = 11;
+            // 
+            // mtbTel
+            // 
+            this.mtbTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbTel.Location = new System.Drawing.Point(271, 214);
+            this.mtbTel.Name = "mtbTel";
+            this.mtbTel.Size = new System.Drawing.Size(408, 30);
+            this.mtbTel.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mtbTel);
+            this.Controls.Add(this.mtbCnpj);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtObs);
+            this.Controls.Add(this.txtNfantasia);
+            this.Controls.Add(this.txtRsocial);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmFormulario3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRsocial;
+        private System.Windows.Forms.TextBox txtNfantasia;
+        private System.Windows.Forms.TextBox txtObs;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox mtbCnpj;
+        private System.Windows.Forms.MaskedTextBox mtbTel;
+    }
+}
+
---
+++ Exercicio3/Exercicio3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if (txtRsocial.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Preencher o campo RAZÃO SOCIAL");
+                txtRsocial.Focus();
+                txtRsocial.Clear();
+            }
+            else if (txtNfantasia.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Preencher o campo NOME FANTASIA");
+                txtNfantasia.Focus();
+                txtNfantasia.Clear();
+            }
+            else if (!mtbCnpj.MaskFull)
+            {
+                MessageBox.Show("Preencher o campo CNPJ");
+                mtbCnpj.Focus();
+                mtbCnpj.Clear();
+            }
+            else if (!mtbTel.MaskFull)
+            {
+                MessageBox.Show("Preencher o campo TELEFONE");
+                mtbTel.Focus();
+                mtbTel.Clear();
+            }
+            else if (txtObs.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Prenecher o campo OBSERVAÇÃO");
+                txtObs.Focus();
+                txtObs.Clear();
+            }
+            else
+            {
+                string rSocial, nFantasia, cnpj, tel, obs;
+                rSocial = txtRsocial.Text;
+                nFantasia = txtNfantasia.Text;
+                cnpj = mtbCnpj.Text;
+                tel = mtbTel.Text;
+                obs = txtObs.Text;
+
+                MessageBox.Show("Razão social: " + rSocial + "\nNome fantasia: " + nFantasia + "\nCnpj: " + cnpj + "\nTelefone: " + tel + "\nObservação: " + obs);
+
+            }
+
+        }
+    }
+}
+       
+
