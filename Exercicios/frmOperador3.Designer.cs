8382f0eee6ff75eaf051e8e3ed58208b0626667a
DianaBarros14 <diana.barros14@gmail.com>
2025-08-03 12:12:58 -0300

    Exercicio 3 operador

---
+++ Exercicios/frmOperador3.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Exercicios
+{
+    partial class frmOperador3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtPorcentagem = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblClassificacao = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite seu salário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Porcentagem de aumento (%):";
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalario.Location = new System.Drawing.Point(289, 52);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(127, 30);
+            this.txtSalario.TabIndex = 1;
+            // 
+            // txtPorcentagem
+            // 
+            this.txtPorcentagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPorcentagem.Location = new System.Drawing.Point(289, 100);
+            this.txtPorcentagem.Name = "txtPorcentagem";
+            this.txtPorcentagem.Size = new System.Drawing.Size(127, 30);
+            this.txtPorcentagem.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(442, 67);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(101, 37);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(567, 58);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(127, 30);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(562, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Novo salário:";
+            // 
+            // lblClassificacao
+            // 
+            this.lblClassificacao.AutoSize = true;
+            this.lblClassificacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClassificacao.Location = new System.Drawing.Point(562, 141);
+            this.lblClassificacao.Name = "lblClassificacao";
+            this.lblClassificacao.Size = new System.Drawing.Size(23, 25);
+            this.lblClassificacao.TabIndex = 7;
+            this.lblClassificacao.Text = "?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(562, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Classificação:";
+            // 
+            // frmOperador3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 187);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblClassificacao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPorcentagem);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmOperador3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmOperador3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtPorcentagem;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblClassificacao;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Exercicios/frmOperador3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicios
+{
+    public partial class frmOperador3 : Form
+    {
+        public frmOperador3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (txtSalario.Text.Trim() == string.Empty || txtPorcentagem.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Preencher todos os campos");
+            }
+            else
+            {
+                decimal salario, aumento, resultado, resultadoSalario;
+                string classificacao = "";
+
+                try 
+                {
+                    salario = Convert.ToDecimal(txtSalario.Text);
+                    aumento = Convert.ToDecimal(txtPorcentagem.Text);
+                    resultado = (salario * aumento) / 100;
+                    resultadoSalario = salario + resultado;
+
+                    if(resultado >= 0 && resultado <= 100)
+                    {
+                        classificacao = "Aumento nível 1";
+                    }
+                    else if(resultado >= 101 && resultado <= 200)
+                    {
+                        classificacao = "Aumento nível 2";
+                    }
+                    else if(resultado >= 201 && resultado <= 300)
+                    {
+                        classificacao = "Aumento nível 3";
+                    }
+                    else if(resultado >= 301 && resultado <= 400)
+                    {
+                        classificacao = "Aumento nível 4";
+                    }
+                    else if(resultado >= 400)
+                    {
+                        classificacao = "Aumento nível 5";
+                    }
+
+                        txtResultado.Text = Convert.ToString(resultadoSalario);
+                        lblClassificacao.Text = classificacao;
+               
+                }
+                catch
+                {
+                    MessageBox.Show("Preencher todos os campos corretamente");
+                }
+            }
+        }
+
+    
+    }
+}
